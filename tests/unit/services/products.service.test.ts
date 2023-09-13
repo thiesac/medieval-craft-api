489d9acb7a06ee082fd8820499e28521864e221e
Thiesa <thiesa.c@gmail.com>
2023-09-13 16:40:40 -0400

    feat: teste do product service

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,30 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import productService from '../../../src/services/product.service'
+import productMock from '../../mocks/product.mock';
 
 describe('ProductsService', function () {
-  beforeEach(function () { sinon.restore(); });
+  beforeEach(function () {
+    sinon.restore();
+  });
 
+  it('should return a successful response when valid data is provided', async function () {
+    const mockProduct = {
+      name: 'Martelo de Thor',
+      price: '30 peças de ouro',
+      orderId: 4,
+    };
+
+    const createStub = sinon.stub().resolves({ dataValues: mockProduct });
+
+    sinon.replace(ProductModel, 'create', createStub);
+
+    const response = await productService.register(mockProduct);
+
+    expect(createStub).to.have.been.calledWith(mockProduct);
+
+    expect(response.status).to.equal('SUCCESSFUL');
+    expect(response.data).to.deep.equal(mockProduct);
+  });
 });
