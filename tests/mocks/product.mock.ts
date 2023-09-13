20d8956c0f9d384518cdfa766ef5a98714718405
Thiesa <thiesa.c@gmail.com>
2023-09-13 16:16:47 -0400

    feat: teste do product controller

---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,13 @@
+const newProductSuccessful = {
+  status: 'SUCCESSFUL' as const, 
+  data: {
+    id: 1,
+    name: 'Martelo de Thor',
+    price: '30 peças de ouro',
+    orderId: 4,
+  },
+};
+
+  export default {
+  newProductSuccessful,
+}
--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,17 +2,41 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productService from '../../../src/services/product.service';
+import productController from '../../../src/controllers/product.controller';
+import productMock from '../../mocks/product.mock';
 
 chai.use(sinonChai);
 
 describe('ProductsController', function () {
-  const req = {} as Request;
-  const res = {} as Response;
+  let req: Request;
+  let res: Response;
 
   beforeEach(function () {
-    res.status = sinon.stub().returns(res);
+    req = {} as Request;
+    res = {} as Response;
+    res.status = sinon.stub().returns(res); // Stub res.status to return res
     res.json = sinon.stub().returns(res);
     sinon.restore();
   });
 
+  it('should return 201 status code and the registered product data on success', async function () {
+    req.body = {
+      name: 'Martelo de Thor',
+      price: '30 peças de ouro',
+      orderId: 4,
+    };
+
+   
+    const productServiceStub = sinon.stub(productService, 'register').resolves(productMock.newProductSuccessful);
+
+    
+    await productController.register(req, res);
+
+   
+    expect(res.status).to.have.been.calledWith(201);
+
+
+    expect(res.json).to.have.been.calledWith(productMock.newProductSuccessful.data);
+  });
 });
