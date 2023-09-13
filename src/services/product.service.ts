7900548bd4d0c7f02a06cb9e186d1fd9320b8cfd
Thiesa <thiesa.c@gmail.com>
2023-09-13 14:45:14 -0400

    feat: implementando camada service de product

---
+++ src/services/product.service.ts
@@ -0,0 +1,35 @@
+import ProductModel, { ProductInputtableTypes } from '../database/models/product.model';
+import { Product } from '../types/Product';
+import { ServiceResponse } from '../types/ServiceResponse';
+
+function validateParams({
+  name,
+  price,
+  orderId,
+}: ProductInputtableTypes): string | null {
+  if (!name) return 'Name is required';
+  if (!price) return 'Price is required';
+  if (!orderId) return 'userId is required';
+
+  /* Se o objeto for válido retorna nulo. */
+  return null;
+}
+
+async function register(product: ProductInputtableTypes): Promise<ServiceResponse<Product>> {
+  let responseService: ServiceResponse<Product>;
+  const error = validateParams(product);
+  
+  if (error) {
+    responseService = { status: 'INVALID_DATA', data: { message: error } };
+    return responseService;
+  }
+
+  const newRegister = await ProductModel.create(product);
+
+  responseService = { status: 'SUCCESSFUL', data: newRegister.dataValues };
+  return responseService;
+}
+
+export default {
+  register,
+};
