1abafa53ae6f84f302d6283f90474dd521c6c7b0
Thiesa <thiesa.c@gmail.com>
2023-09-13 15:26:10 -0400

    feat: rota post de Products funcionando - sem testes

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import productService from '../services/product.service';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+async function register(req: Request, res: Response) {
+  const { name, price, orderId } = req.body;
+  const serviceResponse = await productService.register({ name, price, orderId });
+
+  if (serviceResponse.status !== 'SUCCESSFUL') {
+    return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+  }
+
+  res.status(201).json(serviceResponse.data);
+}
+
+export default {
+  register,
+};
