9506c5875b884eac9f6f8b082f2575115767fe39
Thiesa <thiesa.c@gmail.com>
2023-09-14 10:25:58 -0400

    fix: checagem de senha com bcrypt

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import jwtUtil from '../utils/jwt.util';
 import { ServiceResponse } from '../types/ServiceResponse';
 import UserModel from '../database/models/user.model';
@@ -16,7 +17,7 @@ async function verifyLogin(login: Login): Promise<ServiceResponse<Token>> {
 
   const foundUser = await UserModel.findOne({ where: { username: login.username } });
 
-  if (!foundUser) {
+  if (!foundUser || !bcrypt.compareSync(login.password, foundUser.dataValues.password)) {
     return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
   }
 
