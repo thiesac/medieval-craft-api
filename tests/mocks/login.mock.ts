c4955f0ae6a868f114dc6295d7d207f47b979635
Thiesa <thiesa.c@gmail.com>
2023-09-14 11:48:37 -0400

    feat: testando service login inválido

---
+++ tests/mocks/login.mock.ts
@@ -0,0 +1,10 @@
+const validPassword = '$2a$10$uhkbId.BoDtwlc4/59144.2u7ZnMGo3Z7flfA7hwh0';
+
+const noUsernameLoginBody = {
+  username: '',
+  password: validPassword
+};
+
+export default {
+  noUsernameLoginBody,
+}
--- tests/unit/services/login.service.test.ts
+++ tests/unit/services/login.service.test.ts
@@ -1,7 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import loginMock from '../../mocks/login.mock';
+import loginService from '../../../src/services/login.service';
 
 describe('LoginService', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('#verifyLogin', async function () {
+    const parameters = loginMock.noUsernameLoginBody;
+
+    const serviceResponse = await loginService.verifyLogin(parameters);
+
+    expect(serviceResponse.status).to.equal('INVALID_DATA');
+    expect(serviceResponse.data).not.to.have.key('token');
+    expect(serviceResponse.data).to.deep.eq({ message: '"username" and "password" are required' });
+  });
 });
